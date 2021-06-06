f1f2cda33af4ddfa1438e70accb414ce5593ed37
AndreSmrnv <tehmir@bk.ru>
2021-06-07 02:11:43 +0300

    closeModalByEscKey

--- src/components/modal-overlay/ModalOverlay.jsx
+++ src/components/modal-overlay/ModalOverlay.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import styles from './ModalOverlay.module.css';
 import Modal from "../modal";
@@ -6,6 +6,19 @@ import Modal from "../modal";
 const modalRoot = document.querySelector('#app-modals');
 
 function ModalOverlay({ children, closeModal, header }) {
+
+  const closeModalByEscKey = (e) => {
+    if (e.key === 'Escape')
+      closeModal();
+    e.stopImmediatePropagation();
+  }
+  useEffect(() => {
+    document.addEventListener('keydown', closeModalByEscKey);
+    return () => {
+      document.removeEventListener('keydown', closeModalByEscKey);
+    };
+  }, []);
+
   return ReactDOM.createPortal(
     <div className={styles.modal_overlay} onClick={closeModal} >
       <Modal closeModal={closeModal} modalTitle={header}>
