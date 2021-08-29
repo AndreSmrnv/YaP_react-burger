01123bf87df681975693faa9bc2e85a05521a0ae
AndreSmrnv <tehmir@bk.ru>
2021-08-29 21:40:03 +0300

    viewedItemActions -> ts

---
+++ src/services/actions/viewedItem.ts
@@ -0,0 +1,28 @@
+import type { TIngredient } from '../types/data';
+import {
+    SET_VIEW_ITEM,
+    RESET_VIEW_ITEM
+} from '../constants/actionTypes';
+
+export interface ISetViewItem {
+    readonly type: typeof SET_VIEW_ITEM;
+    readonly payload: Array<TIngredient>;
+  };
+export interface IResetViewItem {
+    readonly type: typeof RESET_VIEW_ITEM;
+  };  
+    
+export type TViewedItemActions = 
+    | ISetViewItem
+    | IResetViewItem
+  ;
+
+export const setViewItem = (data: Array<TIngredient>): TViewedItemActions => ({
+    type: SET_VIEW_ITEM,
+    payload:  data
+  });
+  
+export const resetViewItem = (): TViewedItemActions => ({
+    type: RESET_VIEW_ITEM
+  });
+
