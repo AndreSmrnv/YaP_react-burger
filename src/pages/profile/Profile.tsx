c54132bf348f38baf708515e1f2f950cb87706bc
AndreSmrnv <tehmir@bk.ru>
2021-09-01 22:30:58 +0300

    ProfilePage -> ts

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from '../../services/hooks';
 import { useHistory, useLocation } from 'react-router-dom';
 import { getLogout, getProfile }  from '../../services/actions/sign';
 import { getRefreshToken }  from '../../services/actions';
@@ -13,11 +13,11 @@ import styles from './Profile.module.css';
 import { PROFILE_NAV_TEXT } from '../../services/constants/constValue';
 
 
-function ProfilePage() {
+const ProfilePage: FC = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const location = useLocation();
-  const { isAuthorized, user, lastUpdated, isFetching } = useSelector((store) => store.sign);
+  const { isAuthorized } = useSelector((store) => store.sign);
   const [navText, setNavText] = useState(PROFILE_NAV_TEXT.profile);
   useEffect(
     () => {
