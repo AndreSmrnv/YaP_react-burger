1369d96bd8e5bca37dbc0c1de82362609b2e70ad
AndreSmrnv <tehmir@bk.ru>
2021-09-01 22:32:03 +0300

    RegisterPage -> ts

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-
+import React, { FC } from 'react';
 import {
   Register
 } from '../../components';
 import styles from './Register.module.css';
 
-
-
-function RegisterPage() {
+const RegisterPage: FC = () => {
   return (
     <div className={styles.wrapper}>
       <Register />
     </div>  
   );
 }
-
 
 export default RegisterPage;
