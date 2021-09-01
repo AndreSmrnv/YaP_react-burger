26883a0204931bac3a1c151cbb8ac9ea8c9175f0
AndreSmrnv <tehmir@bk.ru>
2021-09-01 22:14:08 +0300

    NotFound404Page -> ts

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,22 +1,17 @@
-import React, { useEffect } from 'react';
-import { useHistory, Link, useLocation } from 'react-router-dom';
-
+import React, { FC } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './NotFound.module.css';
 
-
-
-export default function NotFound404Page() {
-  const history = useHistory();
-  const { pathname } = useLocation();
-
+const NotFound404Page: FC = () => {
   
-
+  const { pathname } = useLocation();
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         
         <div className={styles.content}>
           <h1>Oops! 404 Error</h1>
+          <h6>{ pathname}</h6>            
           <p>The page you requested does not exist</p>
           <br />
           <br />
@@ -26,3 +21,5 @@ export default function NotFound404Page() {
     </div>
   );
 }
+
+export default NotFound404Page;
