2b6767b508b4ffdb05a8aabd8a71205288e1772d
AndreSmrnv <tehmir@bk.ru>
2021-09-01 22:10:18 +0300

    LoginPage -> ts

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-
+import React, { FC } from 'react';
 import {
   Login
 } from '../../components';
 import styles from './Login.module.css';
 
-
-
-function LoginPage() {
+const LoginPage: FC = () => {
   return (
     <div className={styles.wrapper}>
       <Login />
     </div>  
   );
 }
-
 
 export default LoginPage;
